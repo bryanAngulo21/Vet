05a0d241e339a99ae2258db21991701bed181977
Bryan <andiubra@gmail.com>
2025-12-03 15:54:57 -0500

    Cargar imagenes

--- api-vet/src/controllers/paciente_controller.js
+++ api-vet/src/controllers/paciente_controller.js
@@ -2,7 +2,55 @@ import Paciente from "../models/Paciente.js"
 
 const registrarPaciente = async(req,res)=>{
     try {
-        res.status(200).json({msg:"Registro exitoso de la mascota"})
+        //res.status(200).json({msg:"Registro exitoso de la mascota"})
+
+        //Paso 1 Obtener datos
+        const {emailPropietario} = req.body
+
+
+        //Paso 2 Validar
+            //campos vacios
+        if (Object.values(req.body).includes("")) return res.status(400).json({msg:"Debes llenar todos los campos"})
+
+            //consulta base de datos paa ver usuario registrado
+        const emailExistente = await Paciente.findOne({emailPropietario})
+        if(emailExistente) return res.status(400).json({msg:"El email ya se encuentra registrado"})
+  
+
+        //Paso 3 Logica
+            //crear contraseña temporal para el usuario
+              const password = Math.random().toString(36).toUpperCase().slice(2, 5)
+
+            //guardar datos del usuario 
+            //registro de un nuevo propietario 
+        const nuevoPaciente = new Paciente({
+            ...req.body,//parametrso rest 
+            //encripta el password
+            passwordPropietario: await Paciente.prototype.encryptPassword("VET"+password),
+            //relaciona el veterinario que esta registrando al paciente 
+            veterinario: req.veterinarioHeader._id
+        })            
+        
+        //Imagenes
+        //desde el propietario 
+        if (req.files?.imagen) {
+            const { secure_url, public_id } = await subirImagenCloudinary(req.files.imagen.tempFilePath)
+            nuevoPaciente.avatarMascota = secure_url
+            nuevoPaciente.avatarMascotaID = public_id
+        }
+        
+        //imagenes con ia
+        if (req.body?.avatarMascotaIA) {
+            const secure_url = await subirBase64Cloudinary(req.body.avatarMascotaIA)
+            nuevoPaciente.avatarMascotaIA = secure_url
+        }
+        //guarda  
+        await nuevoPaciente.save()
+        await sendMailToOwner(emailPropietario,"VET"+password)
+
+        //Paso 4 Respuesta
+
+        res.status(201).json({ msg: "Registro exitoso de la mascota y correo enviado al propietario" })
 
     } catch (error) {
         console.error(error)
--- api-vet/src/helpers/uploadCloudinary.js
+++ api-vet/src/helpers/uploadCloudinary.js
@@ -3,9 +3,12 @@ import fs from "fs-extra" //para cargar los archivos
 
 
 // Subir archivos a Cloudinary
+//Funcion necesita el path de la imagen y  la carpeta pacientes
+
 const subirImagenCloudinary = async (filePath, folder = "Pacientes") => {
 
     const { secure_url, public_id } = await cloudinary.uploader.upload(filePath, { folder })
+    //elimina la imagen para que la imagen no permanesca en el back
     await fs.unlink(filePath)
     return { secure_url, public_id }
 
@@ -16,8 +19,11 @@ const subirBase64Cloudinary = async (base64, folder = "Pacientes") => {
 
     // data:image/png;base64,iVBORw0KGgjbjgfyvh
     // iVBORw0KGgjbjgfyvh
+    //quita lo que no necesita 
     const buffer = Buffer.from(base64.replace(/^data:image\/\w+;base64,/, ''), 'base64')
     // iVBORw0KGgjbjgfyvh  -  010101010101010101
+    // convierte a 0 y 1
+    //retorna la url 
     const { secure_url } = await new Promise((resolve, reject) => {
     const stream = cloudinary.uploader.upload_stream({ folder, resource_type: 'auto' }, (err, res) => {
         if (err) reject(err);
