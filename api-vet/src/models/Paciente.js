057cf8041ccda467364f2bdae1de3c40c173a7d9
Bryan <andiubra@gmail.com>
2025-12-03 15:19:37 -0500

    Models paciente

---
+++ api-vet/src/models/Paciente.js
@@ -0,0 +1,112 @@
+import mongoose, {Schema,model} from 'mongoose'
+import bcrypt from "bcryptjs"//encripta contraseñas 
+
+//crar el scquema define los campos del documento 
+const pacienteSchema = new Schema({
+
+    nombrePropietario:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    cedulaPropietario:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    emailPropietario:{
+        type:String,
+        required:true,
+        trim:true,
+        unique: true
+    },
+    passwordPropietario:{
+        type:String,
+        required:true
+    },
+    celularPropietario:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    nombreMascota:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    avatarMascota:{
+        type:String,
+        trim:true
+    },
+    avatarMascotaID:{
+        type:String,
+        trim:true
+    },
+    avatarMascotaIA:{
+        type:String,
+        trim:true
+    },
+    tipoMascota:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    fechaNacimientoMascota:{
+        type:Date,
+        required:true,
+        trim:true
+    },
+    detalleMascota:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    fechaIngresoMascota:{
+        type:Date,
+        required:true,
+        trim:true,
+        default:Date.now
+    },
+    salidaMascota:{
+        type:Date,
+        trim:true,
+        default:null
+    },
+    //puedohacer una eliminacion completa o cambiar el estado
+    estadoMascota:{
+        type:Boolean,
+        default:true
+    },
+    rol:{
+        type:String,
+        default:"paciente"
+    },
+
+
+    veterinario:{
+        //para que se guarde la clave foranea 
+        // ref para referenciar al modelo Veterinario 
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Veterinario'
+    }
+},{
+    //me registra cuando ha sido creado o actualizado 
+    timestamps:true
+})
+
+
+// Método para cifrar el password
+pacienteSchema.methods.encryptPassword = async function(password){
+    const salt = await bcrypt.genSalt(10)
+    return bcrypt.hash(password, salt)
+}
+
+
+// Método para verificar si el password es el mismo de la BDD
+pacienteSchema.methods.matchPassword = async function(password){
+    return bcrypt.compare(password, this.passwordPropietario)
+}
+
+
+// crea un modelo paciente a partir del squema 
+export default model('Paciente',pacienteSchema)
--- api-vet/src/server.js
+++ api-vet/src/server.js
@@ -3,6 +3,10 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
+//Para cluidnari
+import cloudinary from 'cloudinary'
+import fileUpload from "express-fileupload"
+
 // Inicializaciones
 const app = express();
 dotenv.config();
@@ -26,11 +30,41 @@ import routerVeterinarios from './routers/veterinario_routes.js'; // Asegúrate 
 //app.get('/',(req,res)=>res.send("Server on"))
 
 
+// Agregar las rutas de paciente
+import routerPacientes from './routers/paciente_routes.js'
+
+
+
+
 // Rutas para veterinarios
 app.use('/api', routerVeterinarios);
+
+
+// Rutas para pacientes
+app.use('/api',routerPacientes)
 
 // Manejo de una ruta que no sea encontrada (404)
 app.use((req, res) => res.status(404).send("Endpoint no encontrado - 404"));
 
 // Exportar la instancia de express
 export default app;
+
+
+//cloudinary
+
+// Configuraciones
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+
+// Middlewares
+app.use(express.json())
+app.use(cors())
+ 
+app.use(fileUpload({
+    useTempFiles : true,
+    tempFileDir : './uploads'
+}))
